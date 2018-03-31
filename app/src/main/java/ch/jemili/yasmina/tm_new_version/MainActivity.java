f3987ae151393d4f53dea0210a79e0162908ae6e
Yasmina3141 <yayaje1005@gmail.com>
2018-03-31 10:58:17 +0200

    Add the Android part

---
+++ app/src/main/java/ch/jemili/yasmina/tm_new_version/MainActivity.java
@@ -0,0 +1,13 @@
+package ch.jemili.yasmina.tm_new_version;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
