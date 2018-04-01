9920e5bbb60bb45dfdeba7dc01f75884c6d85b45
Yasmina3141 <yayaje1005@gmail.com>
2018-04-01 12:04:30 +0200

    Add Api.java and other minor files

---
+++ app/src/main/java/ch/jemili/yasmina/tm_new_version/Api.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Yasmina on 31.03.18.
+ */
+package ch.jemili.yasmina.tm_new_version;
+
+public class Api {
+    private static final String ROOT_URL = "http://192.168.178.27/TMApi/v1/Api_TM.php?apicall=";
+
+    public static final String URL_CREATE_USER = ROOT_URL + "createuser";
+    public static final String URL_READ_USER = ROOT_URL + "getusers";
+    public static final String URL_UPDATE_USER = ROOT_URL + "updateuser";
+    public static final String URL_DELETE_USER = ROOT_URL + "deleteuser&ID=";
+}
